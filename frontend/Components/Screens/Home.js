b89ff9a1b15a8c062e4b0967b97d091112d0c932
jramirez09 <jramirez12@luc.edu>
2022-09-06 15:03:38 -0600

    added Livepeer Hooks

---
+++ frontend/Components/Screens/Home.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import { Text } from 'react-native';
+
+
+let Home=() =>{
+    return (
+        <Text>Home Page. Will be displaying articles or livestreams</Text>
+    )
+}
